3daaa6cadf56eec7b0c8c78aa30b7a4a542af1ef
mjuniper <mjuniper@gmail.com>
2015-01-30 11:48:08 -0700

    point at staging env

--- app/scripts/app-config.js
+++ app/scripts/app-config.js
@@ -1,4 +1,5 @@
 MyOD.config = {
-  api: '//umb.dcdev.opendata.arcgis.com/'
+  //api: '//umb.dcdev.opendata.arcgis.com/'
   //api: '//data3.dc.opendataqa.arcgis.com/'
+  api: '//umb.dcdev.staging.datacommunity.io/'
 };
