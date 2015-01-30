2f2bafed745e6f7746979f99063575c2c0189593
mjuniper <mjuniper@gmail.com>
2015-01-30 15:03:28 -0700

    use libsass instead of rubysass

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,10 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('styles', function () {
+gulp.task('styles', function() {
   return gulp.src('app/styles/main.scss')
     .pipe($.plumber())
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10
-    }))
+    .pipe($.sass()) 
     .pipe($.autoprefixer({browsers: ['last 1 version']}))
     .pipe(gulp.dest('.tmp/styles'));
 });
