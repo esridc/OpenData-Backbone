c66e4821b2a1302490271dd8514b5ddd6a7e8771
mjuniper <mjuniper@gmail.com>
2015-02-19 13:10:42 -0700

    first stab

--- app/scripts/compiled-templates.js
+++ app/scripts/compiled-templates.js
@@ -28,7 +28,61 @@ __p += '<div class="clearfix">\n  <h2 class="pull-left clearfix"><img class="img
 ((__t = ( tags.join(' | ') )) == null ? '' : __t) +
 '</dd>\n  \n  <dt>Views:</dt>\n  <dd>' +
 ((__t = ( views )) == null ? '' : __t) +
-'</dd>\n\n</dl>\n\n<div id="map"></div>\n<div id="smaps">\n  <div class="container"></div>\n</div>\n';
+'</dd>\n\n</dl>\n\n<div class="container">\n  <div id="map"></div>\n  <div id="smaps">\n    <div class="container"></div>\n  </div>\n</div>\n\n<div id="table-container" class="container"></div>\n';
+
+}
+return __p
+},
+"datasets/templates/table-row": function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+
+ _.each(fields, function (field) { ;
+__p += '\n  <td>' +
+((__t = ( attributes[field.name] )) == null ? '' : __t) +
+'</td>\n';
+ }); ;
+__p += '\n';
+
+}
+return __p
+},
+"datasets/templates/table": function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
+with (obj) {
+__p += '<!-- <h2>Showing...</h2> -->\n<div class="table-responsive">  \n  <table class="table table-striped table-bordered table-hover">\n    <thead>\n      <tr>\n        ';
+ _.each(fields, function (field) { ;
+__p += '\n          <th>' +
+((__t = ( field.alias || field.name )) == null ? '' : __t) +
+'</th>\n        ';
+ }); ;
+__p += '\n      </tr>\n    </thead>\n    <tbody></tbody>\n  </table>\n</div>\n<nav>\n  <ul class="pagination">\n    ';
+ if (pages.length > 1) { ;
+__p += '\n      <li id="page-prev" class="' +
+((__t = ( firstPage )) == null ? '' : __t) +
+'"><a data-page="' +
+((__t = ( prevPage )) == null ? '' : __t) +
+'"><span aria-hidden="true">&laquo;</span></a></li>\n      \n      ';
+ _.each(pages, function (page) { ;
+__p += '\n        <li class="' +
+((__t = ( page.active )) == null ? '' : __t) +
+'"><a class="page-number" data-page="' +
+((__t = ( page.page )) == null ? '' : __t) +
+'">' +
+((__t = ( page.page )) == null ? '' : __t) +
+'</a></li>\n      ';
+ }); ;
+__p += '\n\n      <li id="page-next" class="' +
+((__t = ( lastPage )) == null ? '' : __t) +
+'"><a data-page="' +
+((__t = ( nextPage )) == null ? '' : __t) +
+'"><span aria-hidden="true">&raquo;</span></a></li>\n    ';
+ } ;
+__p += '\n  </ul>\n</nav>';
 
 }
 return __p
---
+++ app/scripts/entities/dataset-row.js
@@ -0,0 +1,16 @@
+
+(function () {
+
+  'use strict';
+    
+  MyOD.module('Models', function (Models, App, Backbone, Marionette, $, _) {
+          
+    Models.DatasetRow = Backbone.Model.extend({
+
+      
+
+    });
+
+  });
+
+})();
