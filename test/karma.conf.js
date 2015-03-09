6e82795319b67aa82dd7729519549eae62880a43
mjuniper <mjuniper@gmail.com>
2015-03-09 15:01:13 -0700

    be explicit about coverage directory for windows

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -68,6 +68,10 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots', 'beep', 'coverage'],
 
+    coverageReporter: {
+      dir: './coverage'
+    },
+
     
     // web server port
     port: 9876,
