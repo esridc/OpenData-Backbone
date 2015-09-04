22d29f95586abfac58d66d0e10f0bddfc04776bb
mjuniper <mjuniper@gmail.com>
2015-09-04 13:13:27 -0600

    refactor mapmanager

--- app/scripts/utils/mapmanager.js
+++ app/scripts/utils/mapmanager.js
@@ -7,13 +7,9 @@
     Utils.MapManager = Marionette.Object.extend({
       
       initialize: function () {
-        _.bindAll(this, 'updateStyle');
-
         this.globalChannel = Backbone.Wreqr.radio.channel('global');
         //load dojo and start the map
         this._loadDojo();
-        App.reqres.setHandler('smaps:update:style', this.updateStyle);
-
       },
 
       /**
@@ -25,9 +21,9 @@
         //check if dojo is loaded...
         if( !window.dojo ){
           //if not, inject a script tag, and then require in things
-          include('//js.arcgis.com/3.13compact/init.js', function(){
+          include('//js.arcgis.com/3.14compact/init.js', function(){
             //now that require is present, load in the other things we need
-            require(['esri/map', 'esri/layers/FeatureLayer', 'plugins/smartMapping', 'dojo/domReady!'], function(Map) {
+            require(['esri/map', 'esri/layers/FeatureLayer', 'dojo/domReady!'], function(Map) {
               dfd.resolve();
             });
           }); 
@@ -53,6 +49,7 @@
 
       createMap: function (mapDiv, options) {
         var self = this;
+
         var mapOpts = {
           center: [ -56.049, 38.485 ],
           zoom: 3,
@@ -66,14 +63,13 @@
 
         this.map = new esri.Map(mapDiv, mapOpts);
         
-       
         var onLoad = function(opts){
-            opts.map.disableScrollWheelZoom();
-            if (options.coords) {
-              var extent = new esri.geometry.Extent(options.coords[0][0], options.coords[0][1], options.coords[1][0], options.coords[1][1], new esri.SpatialReference({ wkid: 4326 }));
-              opts.map.setExtent(extent);
-            }
-            self.proxyEvent('map:load');
+          opts.map.disableScrollWheelZoom();
+          if (options.coords) {
+            var extent = new esri.geometry.Extent(options.coords[0][0], options.coords[0][1], options.coords[1][0], options.coords[1][1], new esri.SpatialReference({ wkid: 4326 }));
+            opts.map.setExtent(extent, true);
+          }
+          self.proxyEvent('map:load');
         };
 
         //proxy events
@@ -81,9 +77,6 @@
         this.map.on('extent-change', dojo.hitch(this, this.proxyEvent, 'map:extent-change'));
         this.map.on('layer-add', dojo.hitch(this, this.proxyEvent, 'map:layer-add'));
       },
-
-
-
 
       getDatasetInfoTemplate: function (dataset) {
         //var datasetName = dataset.get('name');
@@ -100,8 +93,6 @@
           infoTemplate: this.getDatasetInfoTemplate(dataset),
           geometryType: dataset.get('geometry_type')
         };
-        //add the default symbol
-        this._addDefaultSymbols(opts);
         return opts;
       },
 
@@ -109,10 +100,7 @@
         var opts = this.getDatasetLayerOpts(dataset);
         this.datasetLayer = new esri.layers.FeatureLayer(dataset.get('url'), opts);
         //apply default renderer
-        if(opts.layerDefinition && opts.layerDefinition.drawingInfo){
-          //apply renderers
-          this.datasetLayer.setRenderer(this._createRendererFromJson(opts.layerDefinition.drawingInfo.renderer));
-        }
+        this.datasetLayer.setRenderer(this._getRenderer(opts));
 
         this.datasetLayer.on('load', this.onLoadDataset);
 
@@ -123,55 +111,12 @@
         this.datasetLayer.on('update-end', dojo.hitch(this, this.proxyEvent, 'map:datasetlayer:update-end'));
 
         this.map.addLayer(this.datasetLayer);
-
-
       },
 
       onLoadDataset: function (evt) {
         //squash scale ranges - we need the layer to draw at all scales
         evt.layer.minScale = 0; 
         evt.layer.maxScale = 0;
-      },
-
-      updateStyle: function (opts) {
-        var dfd = $.Deferred();
-
-        var layer = opts.layer = this.datasetLayer;
-
-        var _applyRenderer = function(smap){
-          layer.setRenderer(smap.renderer);
-
-          //check if refresh needed, else redraw
-          if ( layer.graphics && layer.graphics.length ) {
-            if (layer.graphics[0].attributes[ opts.field ]) {
-              layer.redraw();
-            } else {
-              layer.refresh();
-            }
-          }
-
-          dfd.resolve(smap.renderer);
-        };
-
-          
-        if (opts.type === 'polygon'){
-          esri.renderer.smartMapping.createClassedColorRenderer(opts)
-            .then(function(renderer){
-              _applyRenderer( renderer );
-            });
-        } else if (opts.type === 'point' && !opts.heatmap){
-          esri.renderer.smartMapping.createClassedSizeRenderer(opts)
-            .then(function(renderer){
-              _applyRenderer( renderer );
-            });
-        } else if (opts.type === 'point' && opts.heatmap) {
-          esri.renderer.smartMapping.createHeatmapRenderer(opts)
-            .then(function(renderer){
-              _applyRenderer( renderer );
-            });
-        }
-
-        return dfd.promise();
       },
 
       /**
@@ -181,51 +126,34 @@
        * @param {Object} layerOptions Json hash of layer properties. Either from a webmap
        * or constructed for a layer in a feature service
        */
-      _addDefaultSymbols: function(layerOptions){
-          //add the layerDefinition node
-          if(!layerOptions.layerDefinition){
-              layerOptions.layerDefinition = {};
-              layerOptions.layerDefinition.drawingInfo = {};
-          }
-          if(!layerOptions.layerDefinition.drawingInfo){
-              layerOptions.layerDefinition.drawingInfo = {};
-          }
+      _getRenderer: function(layerOptions){
+          var renderer;
 
-          //depending on the type, load in the default renderer as json
           switch (layerOptions.geometryType){
               case 'esriGeometryPolygon':
-                  layerOptions.layerDefinition.drawingInfo.renderer = util.defaults.defaultPolygonRenderer;
+                  renderer = this._createRendererFromJson(util.defaults.defaultPolygonRenderer);
                   break;
               case 'esriGeometryPoint':
-                  layerOptions.layerDefinition.drawingInfo.renderer = util.defaults.defaultPointRenderer;
+                  renderer = this._createRendererFromJson(util.defaults.defaultPointRenderer);
                   break;
               case 'esriGeometryMultipoint':
-                  layerOptions.layerDefinition.drawingInfo.renderer = util.defaults.defaultPointRenderer;
+                  renderer = this._createRendererFromJson(util.defaults.defaultPointRenderer);
                   break;
               case 'esriGeometryPolyline':
-                  layerOptions.layerDefinition.drawingInfo.renderer = util.defaults.defaultLineRenderer;
+                  renderer = this._createRendererFromJson(util.defaults.defaultLineRenderer);
                   break;
               case 'esriGeometryLine':
-                  layerOptions.layerDefinition.drawingInfo.renderer = util.defaults.defaultLineRenderer;
+                  renderer = this._createRendererFromJson(util.defaults.defaultLineRenderer);
                   break;
               default: 
-                  layerOptions.layerDefinition.drawingInfo.renderer = util.defaults.defaultPolygonRenderer;
+                  renderer = this._createRendererFromJson(util.defaults.defaultPolygonRenderer);
           }
-          return layerOptions;
+
+          return renderer;
       },
 
       _createRendererFromJson: function(rendererJson){
-          var renderer;
-          switch (rendererJson.type){
-              case 'simple':
-                  //create the default symbol
-                  renderer = new esri.renderer.SimpleRenderer(rendererJson);
-                  break;
-              case 'classBreaks':
-                  renderer = new esri.renderer.ClassBreaksRenderer(rendererJson);
-                  break;
-          }
-          return renderer;
+          return new esri.renderer.SimpleRenderer(rendererJson);
       }
 
       
