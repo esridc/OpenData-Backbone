1572f260ddf8780702364a82ef43b1faf50aa8ad
mjuniper <mjuniper@gmail.com>
2016-02-19 11:57:16 -0700

    fixing tests

--- test/spec/search-model.spec.js
+++ test/spec/search-model.spec.js
@@ -7,15 +7,15 @@ describe('SearchModel', function() {
 
   describe('with defaults', function() {
     beforeEach(function() {
-      
+
     });
 
     it('should properly getRoute', function() {
-      expect(searchModel.getRoute()).toEqual('datasets?q=&page=1&per_page=20&sort_by=relevance');
+      expect(searchModel.getRoute()).toEqual('datasets?q=&page%5Bnumber%5D=1&page%5Bsize%5D=20');
     });
 
     it('should properly getUrl', function() {
-      expect(searchModel.getUrl()).toEqual(MyOD.config.api + 'datasets.json?q=&page=1&per_page=20&sort_by=relevance');
+      expect(searchModel.getUrl()).toEqual(MyOD.config.api + 'datasets?q=&page%5Bnumber%5D=1&page%5Bsize%5D=20');
     });
   });
 
@@ -24,17 +24,16 @@ describe('SearchModel', function() {
       searchModel.set({
         q: 'foo',
         page: 2,
-        sort_by: 'updated_at',
         foo: 'foo'
       });
     });
 
     it('should properly getRoute', function() {
-      expect(searchModel.getRoute()).toEqual('datasets?q=foo&page=2&per_page=20&sort_by=updated_at');
+      expect(searchModel.getRoute()).toEqual('datasets?q=foo&page%5Bnumber%5D=2&page%5Bsize%5D=20');
     });
 
     it('should properly getUrl', function() {
-      expect(searchModel.getUrl()).toEqual(MyOD.config.api + 'datasets.json?q=foo&page=2&per_page=20&sort_by=updated_at');
+      expect(searchModel.getUrl()).toEqual(MyOD.config.api + 'datasets?q=foo&page%5Bnumber%5D=2&page%5Bsize%5D=20');
       //expect(true).toBe(false);
     });
   });
